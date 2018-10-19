120489d0880d9e116d5aa457b267087e4e5b1c5e
abc <abc@example.com>
2018-10-18 21:24:49 -0700

    got the Top 10 results styled and showing. Also the link to track

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
+import Loader from '../layout/Loader'
+import Track from './Track'
+
 
 class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
-          console.log(value);
-          return <h1>Tracks</h1>
+          const { track_list, heading } = value;
+
+          if(track_list === undefined || track_list.length === 0) {
+            return <Loader />
+          }else {
+            return (
+              <React.Fragment>
+                <h4 className="text-center mb-4">{heading}</h4>
+                <div className="row">
+                  {track_list.map(item => (
+                      <Track key={item.track.track_id} track={item.track } />
+                    ))}
+                </div>
+              </React.Fragment>
+            )
+          }
         }}
       </Consumer>
     )
